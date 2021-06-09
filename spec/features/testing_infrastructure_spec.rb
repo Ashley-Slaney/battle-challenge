0a56e88349db7b7bf0162f0343590275470647b4
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-09 17:18:57 +0100

    shows player 2s hp

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,12 +1,23 @@
 feature 'Enter names' do
   scenario 'Player can enter names and submit the form' do
     visit('/')
-    fill_in :player_1, with: 'player one'
-    fill_in :player_2, with: 'player two'
+    fill_in :player_1, with: 'Ash'
+    fill_in :player_2, with: 'Frank'
     click_on 'Submit'
 
     save_and_open_page
 
-    expect(page).to have_content('player one VS player two')
+    expect(page).to have_content('Ash VS Frank')
   end
 end
+
+feature 'View hit points' do
+  scenario 'view player 2s hit points' do
+    visit('/')
+    fill_in :player_1, with: 'Ash'
+    fill_in :player_2, with: 'Frank'
+    click_on 'Submit'
+
+    expect(page).to have_content('Frank: 100HP')
+  end   
+end 
