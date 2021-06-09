1378dfff4d67bfa0f0497a532b34f7ee84b4b5c7
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-09 17:31:17 +0100

    refactored with test helpers

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,11 +1,8 @@
 feature 'Enter names' do
   scenario 'Player can enter names and submit the form' do
-    visit('/')
-    fill_in :player_1, with: 'Ash'
-    fill_in :player_2, with: 'Frank'
-    click_on 'Submit'
+    sign_in_and_play
 
-    save_and_open_page
+    # save_and_open_page
 
     expect(page).to have_content('Ash VS Frank')
   end
@@ -13,10 +10,7 @@ end
 
 feature 'View hit points' do
   scenario 'view player 2s hit points' do
-    visit('/')
-    fill_in :player_1, with: 'Ash'
-    fill_in :player_2, with: 'Frank'
-    click_on 'Submit'
+    sign_in_and_play
 
     expect(page).to have_content('Frank: 100HP')
   end   
